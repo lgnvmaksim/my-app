ae27cf92cf47c721420724e9a04e4f840c3d6af9
lgnvmaksim <lgnvmaksim@gmail.com>
2022-09-26 17:18:52 +0300

    Comit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Lorem3 sds orr</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Learn React
         </a>
       </header>
     </div>
